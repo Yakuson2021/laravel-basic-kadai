0bdb68b4cc5a918d02b0b4f2df71cf1410d0cb26
原 恭彦 <yahara@yahoo-corp.jp>
2024-06-22 19:46:42 +0900

    Initial commit for laravel-basic-kadai3

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 // ルーティングを設定するコントローラを宣言する
-use App\Http\Controllers\HelloController;
+use App\Http\Controllers\Posts\PostController; 
 
 
 /*
@@ -24,4 +24,4 @@ Route::get('/hello', function () {
     return 'あああ';
 });
 
-Route::get('/posts', [HelloController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
